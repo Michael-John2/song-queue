f4e2cceecc4682fd096a2ee99726cccbf782d7a7
John Michael Dalaguan <mchinmichael@gmail.com>
2026-05-17 22:40:11 +0800

    just reformatting and adding json files to launch

---
+++ README.md
@@ -0,0 +1,25 @@
+# song-queue
+Music Playlist using Linked List in C
+
+March 31 "No progress made at the moment"
+
+April 1 11pm "Successfully created a git and connecting it to my github account alongside the creation of the file and submitting a url link for monitoring work progress"
+
+
+April 2 9:31pm "Still Made some little progress"
+
+April 3 "No progress made at the moment"
+
+April 4 4:20pm "Analyzing the flowchart work and making some bit of modification and fixing on some issues to it but other than that still making little progress"
+
+
+April 5 12:15AM
+"currently workin on the 3 files such as c codes of header and implementation file and also main file while also arranging the files in src folder to be organized and not messy"
+New update:
+"Errors occurred on running the file, working on finding the root cause and implementing solutions to it"
+
+April 26-27
+"Fixing my visual studio as it cant run the code and forced to run MSVC Compiler error "MSB4025", while also checking my code and makin changes to it"
+
+April 27 9:06pm
+"After hours of confusion and erros, code finally worked"
---
+++ src/song_queue.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char title[100];
+    char artist[100];
+    float duration;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    char title[100];
+    char artist[100];
+    float duration;
+} Song;
+
+Song library[10] = {
+    {"Animals",       "Maroon 5",            3.50},
+    {"Sacrifice",                 "League of Legends, G.E.M.",      4.08},
+    {"Bring Me To Life",        "Evanescence",           3.55},
+    {"Devil Trigger",           "Casey Edwards",      6.45},
+    {"Can You Feel My Heart", "Bring Me The Horizon",          3.47},
+    {"Sexy And I Know It",             "LMFAO",  3.19},
+    {"Bismark",      "Zabaton",     5.13},
+    {"Thunderstruck",    "AC/DC",       4.52},
+    {"Pasilyo",  "Sunkissed Lola",  4.29},
+    {"Fortunate Son",     "Creedence Clearwater Revival",    2.19}
+};
+
+void displayLibrary(Song lib[], int size);
+void displayMenu();
+void addSong(Node **head, Node **tail, Song lib[]);
+void viewPlaylist(Node *head);
+void playNext(Node **head, Node **tail);
+void totalDuration(Node *head);
+
+int main() {
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    printf("\n+==============================+\n");
+    printf("|   Music Playlist Manager     |\n");
+    printf("+==============================+\n");
+
+    displayLibrary(library, 10);
+
+    int choice;
+    do {
+        displayMenu();
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1: addSong(&head, &tail, library);  break;
+            case 2: viewPlaylist(head);               break;
+            case 3: playNext(&head, &tail);           break;
+            case 4: totalDuration(head);              break;
+            case 5:
+                printf("\nGoodbye!\n\n");
+                break;
+            default:
+                printf("\n  Invalid choice. Try again.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
+
+void displayLibrary(Song lib[], int size) {
+    printf("\n--- Song Library ---\n");
+    for (int i = 0; i < size; i++) {
+        printf("  [%2d] %-30s - %s\n", i + 1, lib[i].title, lib[i].artist);
+    }
+    printf("--------------------\n");
+}
+
+void displayMenu() {
+    printf("\n------ MENU ------\n");
+    printf("  1. Add Song\n");
+    printf("  2. View Playlist\n");
+    printf("  3. Play Next Song\n");
+    printf("  4. Show Total Duration\n");
+    printf("  5. Exit\n");
+    printf("------------------\n");
+}
+
+void addSong(Node **head, Node **tail, Song lib[]) {
+    displayLibrary(lib, 10);
+    printf("\n  Enter song number (1-10): ");
+    int num;
+    scanf("%d", &num);
+    getchar();
+
+    if (num < 1 || num > 10) {
+        printf("  Invalid number.\n");
+        return;
+    }
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("  Memory allocation failed.\n");
+        return;
+    }
+
+    strcpy(newNode->title,  lib[num - 1].title);
+    strcpy(newNode->artist, lib[num - 1].artist);
+    newNode->duration = lib[num - 1].duration;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        *tail = newNode;
+    } else {
+        (*tail)->next = newNode;
+        *tail = newNode;
+    }
+
+    printf("\n  Song Added: \"%s\" by %s\n", newNode->title, newNode->artist);
+}
+
+void viewPlaylist(Node *head) {
+    printf("\n=== Your Playlist ===\n");
+
+    if (head == NULL) {
+        printf("  (Playlist is empty)\n");
+        return;
+    }
+
+    Node *current = head;
+    int i = 1;
+    while (current != NULL) {
+        printf("  [%d] %-30s - %-20s %.2f min\n",
+               i++, current->title, current->artist, current->duration);
+        current = current->next;
+    }
+    printf("=====================\n");
+}
+
+void playNext(Node **head, Node **tail) {
+    if (*head == NULL) {
+        printf("\n  Playlist is empty. Add songs first!\n");
+        return;
+    }
+
+    Node *temp = *head;
+    *head = (*head)->next;
+
+    printf("\n  >> Now Playing: \"%s\" by %s (%.2f min)\n",
+           temp->title, temp->artist, temp->duration);
+
+    free(temp);
+
+    if (*head == NULL) {
+        *tail = NULL;
+        printf("  (Playlist is now empty)\n");
+    }
+}
+
+void totalDuration(Node *head) {
+    if (head == NULL) {
+        printf("\n  Playlist is empty.\n");
+        return;
+    }
+
+    float total = 0;
+    Node *current = head;
+    while (current != NULL) {
+        total += current->duration;
+        current = current->next;
+    }
+
+    printf("\n  Total Playlist Duration: %.2f minutes\n", total);
+}
